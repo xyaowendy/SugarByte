5f1844ed06b53c6b827a5d85d6a957bcfca02ec4
wendyjiang0611 <wendyjiang0611@gmail.com>
2019-09-04 07:18:37 +0000

    Updated bin/paddock_manager.js (added 1 line, deleted 1 line).

--- bin/paddock_manager.js
+++ bin/paddock_manager.js
@@ -4,7 +4,7 @@
 
 var manager = {};
 
-var debug = require('users/balddinosaur/sugarbyte:bin/debug');
+var debug = require('users/balddinosaur/sugarbyte:bin/debug.js');
 
 /**
  * Instantiate the paddock outline visualiser tool.
