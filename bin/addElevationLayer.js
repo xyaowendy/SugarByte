8820995c0067bdd8d92a4d877da126fa555277f4
ljs.upc.uq <ljs.upc.uq@gmail.com>
2019-09-16 11:11:59 +0000

    Updated bin/addElevationLayer.js (added 1 line, deleted 1 line).

--- bin/addElevationLayer.js
+++ bin/addElevationLayer.js
@@ -31,6 +31,7 @@ var elevationTestChart = ui.Chart.image.byRegion({
   scale: 200,
   xProperty: 'name'
 });
+
 elevationTestChart.setOptions({
   title: 'Elevation test chart',
   vAxis: {
@@ -45,7 +46,6 @@ print(elevationTestChart);
 
 // elevation = elevation.setName("hansen1"); 
 // elevation = elevation.setOpacity(0.5);
-
 
 // layer 1 = layer 1.setName("hansen1"); 
 // layer 1 = layer 1.setOpacity(0.5); 
