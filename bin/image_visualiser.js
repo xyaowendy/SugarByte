079af14e92c047f0c1c8f2714e5bc7f703a7cfa4
yiheng.zhang678 <yiheng.zhang678@gmail.com>
2019-09-16 03:29:19 +0000

    Updated bin/image_visualiser.js (added 1 line, deleted 1 line).

--- bin/image_visualiser.js
+++ bin/image_visualiser.js
@@ -9,7 +9,7 @@ var manager = {};
 // timezone constant, may wish to migrate to app constants later
 var TIMEZONE = 'Australia/Brisbane';
 
-// var selectedPaddock = app.pa
+// var selectedPaddock = ?
 
 var debug = require('users/balddinosaur/sugarbyte:bin/debug.js');
 
