2b55e8e612ba4d933c731d1eea833c42759d1954
huiyuzeng <huiyu.zeng@uqconnect.edu.au>
2019-09-16 22:32:38 +1000

    link_image_version 3.3

--- bin/ui_widgets/information_panel_factory.js
+++ bin/ui_widgets/information_panel_factory.js
@@ -182,9 +182,9 @@ var createNDVIVisualiser = function(paddock) {
       // Get the 5 day range (guarantees that at least one data point will be present
       var dateRange = ee.DateRange(date, date.advance(5, 'day'));
 
-      // clear all ndvi layers before displaying new one
+      // clear all NDVI layers before displaying new one
       manager.app.imageVisualiser.clearAllNdviLayers();
-      //visualizing ndvi of chosen time point of scatter chart on the map
+      //visualizing NDVI of chosen time point of scatter chart on the map
       manager.app.imageVisualiser.displayPaddockNDVIMedian(
           dateRange.start(),
           dateRange.end(),
@@ -194,7 +194,7 @@ var createNDVIVisualiser = function(paddock) {
           true);
       // Show a label with the date on the map.
       manager.time_lable.setValue(new Date(xValue).toUTCString());
-      debug.info("added image to time series", date);
+      debug.info("added NDVI imagery to time series", date);
     })
   };
 
