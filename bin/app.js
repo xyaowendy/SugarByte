6e6fb77826664bed9ea65991ef22eeabfd706913
huiyuzeng <huiyu.zeng@uqconnect.edu.au>
2019-09-17 19:23:42 +1000

    link_image_version 3.31

--- bin/app.js
+++ bin/app.js
@@ -46,7 +46,7 @@ var createConstants = function() {
   app.default = {
     CHART_START_DATE: '2018-01-01',
     CHART_END_DATE: date.getFullYear()+'-0'+date.getMonth()+'-'+date.getDate(), // month needs +1
-    MAP_ZOOM: 13,
+    MAP_ZOOM: 15,
     MAP_ZOOM_SELECTED: 16,
     mapCoordinates: {
       LON: 145.9222,
